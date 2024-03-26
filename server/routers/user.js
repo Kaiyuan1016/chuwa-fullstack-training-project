2e59e48e62412239c87a97a6462e981d71bf3b27
Yaqian Yang <ttyihark7@gmail.com>
2024-03-25 23:13:39 -0700

    delete unnecessary routers folder

--- server/routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createUser, updatePassword } = require("../../controllers/user");
-const router = express.Router();
-
-
-router.post("/", createUser);
-router.put("/", updatePassword);
-
-module.exports = router;
