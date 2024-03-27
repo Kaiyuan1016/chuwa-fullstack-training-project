27cdce2d7f451f8dd7486ac9d29173e2af06b1a6
Kaiyuan Wang <kaiyuanwang2089@gmail.com>
2024-03-27 16:32:12 -0700

    Create Cart.js

---
+++ Backend/models/Cart.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [cartItemSchema]
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+module.exports = Cart;
