600644ec43794ff9111cac6ca5d88247591d9f1f
Kaiyuan Wang <kaiyuanwang2089@gmail.com>
2024-03-28 21:06:48 -0700

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
+
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./routers/user')
+const productRouter = require('./routers/product');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/', productRouter);
+app.use('/', userRouter);
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
