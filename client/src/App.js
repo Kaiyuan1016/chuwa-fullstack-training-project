c973fcd54cead8078d45610c3a4ad2e5f4f069e8
Kaiyuan Wang <kaiyuanwang2089@gmail.com>
2024-03-29 16:24:58 -0700

    sign up page updated

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import ProductDetail from './ProductDetail';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ProductsPage from './ProductsPage';
+import SignUp from './pages/Signup';
 
 export default function App () {
   return(
@@ -14,6 +15,7 @@ export default function App () {
         <Route path='/' element={<ProductsPage />} />
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/products/:productId' element={<ProductDetail />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+import SignupForm from "../components/SignupForm";
+import { Stack } from "@mui/material";
+import { useUserForm } from "../hooks/useUserForm";
+
+const SignUp = () => {
+  const userFormHooks = useUserForm();
+  return (
+    <Stack justifyContent="center" alignItems="center">
+      <SignupForm userFormHooks={userFormHooks} />
+    </Stack>
+  );
+};
+
+export default SignUp;
+
